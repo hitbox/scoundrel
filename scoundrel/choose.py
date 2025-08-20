64d18734508d0969701ccba89bff18fbac442b40
Carl Harris <hitbox@ryzen.com>
2025-08-20 13:39:59 -0400

    Added menu class.

---
+++ scoundrel/choose.py
@@ -0,0 +1,51 @@
+class ChooseMenu:
+
+    def __init__(self, indexed_choices):
+        self.indexed_choices = indexed_choices
+        self.chosen_indexes = set()
+        self.unavailable = set()
+
+    def pick(self, index):
+        """
+        Add explicitly chosen menu item by index.
+        """
+        self.chosen_indexes.add(index)
+
+    def is_available(self, index):
+        """
+        Index is not already chosen or made unavailable.
+        """
+        return index not in self.chosen_indexes and index not in self.unavailable
+
+    def menu_lines(self):
+        """
+        Return list of tuples (index, value, label) for current choices.
+        """
+        menu = []
+
+        for index, value, label in self.indexed_choices:
+            if self.is_available(index):
+                # Available option
+                menu.append((index, value, label))
+            else:
+                # Blank line index
+                menu.append((None, None, label))
+
+        return menu
+
+    def update_for_available(self, available_choices):
+        """
+        Update current choices, making them unavailable if not in given choices.
+        """
+        available_values = set(value for value, label in available_choices)
+        for index, value, label in self.indexed_choices:
+            if value not in available_values:
+                self.unavailable.add(index)
+
+    def value_for_index(self, chosen):
+        """
+        Return value for given index.
+        """
+        for index, value, _ in self.indexed_choices:
+            if index == chosen:
+                return value
--- scoundrel/view.py
+++ scoundrel/view.py
@@ -1,5 +1,6 @@
 import sys
 
+from .choose import ChooseMenu
 from .util import letter_indexer
 
 INDENT = '  '
@@ -9,8 +10,12 @@ class ScoundrelTUI:
     Scoundrel text user interface.
     """
 
-    def __init__(self, stream=None):
+    def __init__(self, stream=None, menu_class=None):
         self._stream = stream
+
+        if menu_class is None:
+            menu_class = ChooseMenu
+        self.menu_class = menu_class
 
     @property
     def stream(self):
@@ -25,7 +30,7 @@ class ScoundrelTUI:
         game.on('play_card', self.generic_alert)
         game.on('ran_away', self.generic_alert)
         game.on('game_over', self.game_over_alert)
-        game.on('init_room', self.alert_init_room)
+        game.on('init_room', self.init_room)
         game.on('heal', self.alert_heal)
         game.on('ran_away', self.alert_ran_away)
 
@@ -35,8 +40,17 @@ class ScoundrelTUI:
     def alert_heal(self, event_name, game, amount, card):
         self.stream.write(f'\nYou healed for {amount}.\n')
 
-    def alert_init_room(self, event_name, game):
+    def init_room(self, event_name, game):
+        """
+        Alert for new room and build menu.
+        """
         self.stream.write(f'\nYou step into a new room.\n')
+        indexed_choices = [
+            (index, value, label)
+            for index, (value, label)
+            in letter_indexer(game.choices_for_turn())
+        ]
+        self.room_menu = self.menu_class(indexed_choices)
 
     def alert_damage_taken(self, event_name, game, damage, source, weapon):
         if damage <= 0:
@@ -57,36 +71,45 @@ class ScoundrelTUI:
         """
         Cards in play on the battlefield.
         """
-        if not game.battlefield_deck:
+        if not game.decks.cards('battlefield'):
             self.stream.write('\nThe battlefield awaits you\n')
         else:
             self.stream.write('\nBattlefield\n')
-            for card in game.battlefield_deck:
+            for card in game.decks.cards('battlefield'):
                 self.stream.write(f'{INDENT}{card.game_string}\n')
+
+    def prompt_for_turn(self, game, available_choices):
+        """
+        Build choices for a turn of scoundrel and prompt until valid response.
+        """
+        # Update menu for available choices.
+        self.room_menu.update_for_available(available_choices)
+
+        # Loop until valid choice.
+        while True:
+            self.stream.write('\nChoose card from room\n')
+            indexed = self.room_menu.menu_lines()
+            for index, value, label in indexed:
+                if index is None:
+                    index = ' '
+                self.stream.write(f'({index}) {label}\n')
+
+            response = input(f'Health x {game.health}> ')
+
+            if response in self.room_menu.chosen_indexes:
+                self.stream.write(f'Already played\n')
+            elif response in self.room_menu.unavailable:
+                self.stream.write(f'Unavailable\n')
+            else:
+                value = self.room_menu.value_for_index(response)
+                if value:
+                    self.room_menu.pick(index)
+                    return value
+
+                self.stream.write(f'Invalid input: {response}\n')
 
     def game_over_alert(self, event_name, game):
         if game.is_player_alive:
             self.stream.write('You win!\n')
         else:
             self.stream.write('You died\n')
-
-    def prompt_for_turn(self, game, choices):
-        """
-        Build choices for a turn of scoundrel and prompt until valid response.
-        """
-        prompt = f'Health x {game.health}> '
-
-        indexed = letter_indexer(choices)
-        indexed = [(index, value, label) for index, (value, label) in indexed]
-
-        while True:
-            self.stream.write('\nChoose card from room\n')
-            for index, _, label in indexed:
-                self.stream.write(f'({index}) {label}\n')
-
-            response = input(prompt)
-            for index, value, _ in indexed:
-                if response == index:
-                    return value
-            else:
-                self.stream.write(f'Invalid input: {response}\n')
